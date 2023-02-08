fcb69634d4d2a90bd34d63352e3152e6f7f6db02
mikiasano28 <shiro.kume.30s2828@gmail.com>
2023-02-09 01:09:48 +0900

    update App.js set cleanup function

--- src/App.js
+++ src/App.js
@@ -1,44 +1,45 @@
 import React, { useState, useEffect } from 'react';
 
 
-const App = () => {
-  const [time, setTime] = useState(0);
-
-  // コンポーネントが生成されたときに一回だけ呼びたいときは、useEffectのcallback関数内で呼びたい処理を記述する。その際は第２引数に空の配列をつけるのを忘れないこと
-
-
-  // useEffect(() => {
-  //   console.log("useEffect is called");
-  //   window.setInterval(() => {
-  //     setTime(prev => ++prev);
-  //   }, 1000);
-  // }, []);   
-
-
-  //　空の配列を渡すことに良lってExampleコンポーネントが呼ばれた時のみsetInterval関数が実行される
-  // 空の配列　-> 依存してる関数がない
-
-
-  // useEffect(() => {
-  //   console.log("updated!");
-  // }, [time]);
-
-
-  // 依存配列に含めたstateを更新すると、このcallback関数が再度実行される
-
-  // window.setInterval(() => {
-  //   // setTime(prev => ++prev);
-  // }, 1000);
+const Example = () => {
+  const [isDisp, setIsDisp] = useState(true);
 
   return (
-    <h1>
+    <>
+      {isDisp && <Timer />}
+      <button onClick={() => setIsDisp(prev => !prev)}>Toggle</button>
+    </>
+  );
+}
+
+const Timer = () => {
+  const [time, setTime] = useState(0);
+
+  useEffect(() => {
+    // console.log("updated");
+    let intervalId = null;
+    intervalId = window.setInterval(() => {
+      console.log("interval called");
+      setTime(prev => prev + 1);
+    }, 1000);
+
+    return () => {
+      // debugger
+      // console.log("updated end");   // updatedを初期化するために呼ばれていて、その後またupdatedが呼ばれる
+      window.clearInterval(intervalId);
+    }
+  }, [time])
+
+
+  return (
+    <h2>
       <time>{time}</time>
       <span> seconds</span>
-    </h1>
+    </h2>
   );
 
 }
 
 
-export default App;
 
+export default Example;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-
+import Example from './App';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Example />
   </React.StrictMode>
 );
 
